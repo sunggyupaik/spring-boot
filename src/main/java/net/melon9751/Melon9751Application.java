0b73f7e1defa7ebbe319dfcae3f4d026ddbfb98a
melon9751 <melon9751@naver.com>
2020-04-23 17:06:34 +0900

    upload

---
+++ src/main/java/net/melon9751/Melon9751Application.java
@@ -0,0 +1,13 @@
+package net.melon9751;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Melon9751Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Melon9751Application.class, args);
+	}
+
+}
---
+++ src/test/java/net/melon9751/Melon9751ApplicationTests.java
@@ -0,0 +1,13 @@
+package net.melon9751;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Melon9751ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
