36451776b7e5870b8c1cac8dacc5639f8046425e
melon9751 <melon9751@naver.com>
2020-04-29 16:32:10 +0900

    질문하기 기능 구현 - 수정(1)

--- src/main/java/net/melon9751/web/UserController.java
+++ src/main/java/net/melon9751/web/UserController.java
@@ -37,21 +37,27 @@ public class UserController {
 			return "rediret:/users/loginForm";
 		}
 
+		//값을 꺼낼려고 하지 말고, 메시지를 보내 확인하게 한다.(객체지향적설계)
+//		if (!password.equals(user.getPassword())) {
+//			System.out.println("Login Failed");
+//			return "rediret:/users/loginForm";
+//		}
+		
 		// 비밀번호가 다르면 다시 로그인창
-		if (!password.equals(user.getPassword())) {
+		if (!user.matchPassword(password)) {
 			System.out.println("Login Failed");
 			return "rediret:/users/loginForm";
 		}
 
 		// 로그인이 정상적으로 되면 session에 user 정보 저장
 		System.out.println("Login Success");
-		Session.setAttribute("sessionedUser", user);
+		Session.setAttribute(HttpSessionUtils.USER_SESSION_KEY, user);
 		return "redirect:/";
 	}
 
 	@GetMapping("logout")
 	public String logout(HttpSession session) {
-		session.removeAttribute("sessionedUser");
+		session.removeAttribute(HttpSessionUtils.USER_SESSION_KEY);
 		return "redirect:/";
 	}
 
@@ -62,30 +68,27 @@ public class UserController {
 
 	@PostMapping("")
 	public String create(User user) {
-		System.out.println("user" + user);
 		userRepository.save(user);
 		return "redirect:/users";
 	}
 
 	@GetMapping("")
 	public String list(Model model) {
+		System.out.println("list에 들어가기 전");
 		model.addAttribute("users", userRepository.findAll());
-		return "/users/list";
+		return "/user/list";
 	}
 
-	// "개인정보 수정" 클릭 시,
+	// "개인정보 수정"을 위한 버튼 클릭 시,
 	@GetMapping("/{Id}/form")
 	public String updateForm(@PathVariable Long Id, Model model, HttpSession session) {
-		Object tempUser = session.getAttribute("sessionedUser");
-		if (tempUser == null) {
+		if (!HttpSessionUtils.isLoginUser(session)) {
 			return "redirect:/users/loginForm";
 		}
-
-		User sessionUser = (User) tempUser;
-		if(!(sessionUser.getId() == Id)) {
+		User sessionedUser = HttpSessionUtils.getUserfromSession(session);
+		if(!sessionedUser.matchId(Id)) {
 			throw new IllegalStateException("자신의 정보만 수정 할 수 있습니다.");
 		}
-		
 		User user = userRepository.findById(Id).get();
 		model.addAttribute("user", user);
 		return "/user/updateForm";
@@ -94,19 +97,20 @@ public class UserController {
 	// "개인정보 수정"을 완료하기 확인 시,
 	@PostMapping("/{Id}")
 	public String update(@PathVariable Long Id, User updatedUser, HttpSession session) {
-		Object tempUser = session.getAttribute("sessionedUser");
-		if (tempUser == null) {
+		if (!HttpSessionUtils.isLoginUser(session)) {
 			return "redirect:/users/loginForm";
 		}
-
-		User sessionUser = (User) tempUser;
-		if(!(sessionUser.getId() == Id)) {
+		
+		User sessionedUser = HttpSessionUtils.getUserfromSession(session);
+		if(!sessionedUser.matchId(Id)) {
 			throw new IllegalStateException("자신의 정보만 수정 할 수 있습니다.");
 		}
 		
+		System.out.println("UpdatedUser : " +updatedUser);
 		User user = userRepository.findById(Id).get();
+		System.out.println("beforeUser : " + user);
 		user.update(updatedUser);
 		userRepository.save(user);
-		return "/redirect:/users";
+		return "redirect:/users";
 	}
 }
